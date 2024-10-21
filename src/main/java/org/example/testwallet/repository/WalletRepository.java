667201620c9982de85b6349b3dc88d4d762e41a9
alexeystolyarov <stolyarov_aleksei@mail.ru>
2024-10-21 19:07:10 +0300

    repo

--- src/main/java/org/example/testwallet/repository/WalletRepository.java
+++ src/main/java/org/example/testwallet/repository/WalletRepository.java
@@ -2,8 +2,10 @@ package org.example.testwallet.repository;
 
 import org.example.testwallet.model.Wallet;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface WalletRepository extends JpaRepository<Wallet, UUID> {
 }
